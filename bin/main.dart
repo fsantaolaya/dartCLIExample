7e396cea83ca0b18f570fc32d1267d7cb9f3956d
xe52360 <fernando.santa.contractor@bbva.com>
2016-09-18 13:21:58 +0200

    Version with help

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,25 @@
 // Copyright (c) 2016, xe52360. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-
+import 'package:args/args.dart';
 import 'package:dartCLIExample/dartCLIExample.dart' as dartCLIExample;
 
-main(List<String> arguments) {
-  print('Hello world: ${dartCLIExample.calculate()}!');
+ArgResults argResults;
+
+void main(List<String> arguments) {
+  final ArgParser argParser = new ArgParser()
+    ..addOption('name', abbr: 'n', defaultsTo: 'World',
+        help: "Who would you like to greet?")
+    ..addFlag('help', abbr: 'h', negatable: false,
+        help: "Displays this help information.");
+
+  argResults = argParser.parse(arguments);
+
+  final String name = argResults['name'];
+  if (argResults['help']) {
+    print("""
+** HELP **
+${argParser.usage}
+    """);
+  }
+  print("Hello, $name!, is  ${dartCLIExample.calculate()}");
 }
